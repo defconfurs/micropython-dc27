4c7eceec3dc4f02b710a695516dcdf3e0cd4c132
Owen Kirby <oskirby@gmail.com>
2019-06-13 16:11:03 -0700

    Initial board port to the DCFurs DC27 badge.

---
+++ ports/stm32/boards/DCFURS_L475/mpconfigboard.h
@@ -0,0 +1,64 @@
+#define MICROPY_HW_BOARD_NAME       "DCFURS"
+#define MICROPY_HW_MCU_NAME         "STM32L475"
+
+#define MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE (0)
+#define MICROPY_HW_HAS_SWITCH       (1)
+#define MICROPY_HW_HAS_FLASH        (1)
+#define MICROPY_HW_ENABLE_RNG       (1)
+#define MICROPY_HW_ENABLE_USB       (1)
+
+// use external SPI flash for storage
+#define MICROPY_HW_SPIFLASH_SIZE_BITS (32 * 1024 * 1024)
+#define MICROPY_HW_SPIFLASH_CS      (pin_A4)
+#define MICROPY_HW_SPIFLASH_SCK     (pin_A5)
+#define MICROPY_HW_SPIFLASH_MISO    (pin_A6)
+#define MICROPY_HW_SPIFLASH_MOSI    (pin_A7)
+
+// block device config for SPI flash
+extern const struct _mp_spiflash_config_t spiflash_config;
+extern struct _spi_bdev_t spi_bdev;
+#define MICROPY_HW_BDEV_IOCTL(op, arg) ( \
+    (op) == BDEV_IOCTL_NUM_BLOCKS ? (MICROPY_HW_SPIFLASH_SIZE_BITS / 8 / FLASH_BLOCK_SIZE) : \
+    (op) == BDEV_IOCTL_INIT ? spi_bdev_ioctl(&spi_bdev, (op), (uint32_t)&spiflash_config) : \
+    spi_bdev_ioctl(&spi_bdev, (op), (arg)) \
+)
+#define MICROPY_HW_BDEV_READBLOCKS(dest, bl, n) spi_bdev_readblocks(&spi_bdev, (dest), (bl), (n))
+#define MICROPY_HW_BDEV_WRITEBLOCKS(src, bl, n) spi_bdev_writeblocks(&spi_bdev, (src), (bl), (n))
+
+// MSI is used and is 4MHz
+#define MICROPY_HW_CLK_PLLM (1)
+#define MICROPY_HW_CLK_PLLN (40)
+#define MICROPY_HW_CLK_PLLP (RCC_PLLP_DIV7)
+#define MICROPY_HW_CLK_PLLR (RCC_PLLR_DIV2)
+#define MICROPY_HW_CLK_PLLQ (RCC_PLLQ_DIV2)
+
+#define MICROPY_HW_FLASH_LATENCY    FLASH_LATENCY_4
+
+// USART3 is connected to the BLE module
+#define MICROPY_HW_UART3_TX     (pin_B10)
+#define MICROPY_HW_UART3_RX     (pin_B11)
+
+// I2C busses
+#define MICROPY_HW_I2C3_SCL (pin_C0)
+#define MICROPY_HW_I2C3_SDA (pin_C1)
+
+
+// User and wake-up switch. Pressing the button makes the input go low.
+#define MICROPY_HW_USRSW_PIN        (pin_C14)
+#define MICROPY_HW_USRSW_PULL       (GPIO_NOPULL)
+#define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_RISING)
+#define MICROPY_HW_USRSW_PRESSED    (1)
+
+// LEDs
+#define MICROPY_HW_LED_INVERTED     (1) // LEDs are on when pin is driven low
+#define MICROPY_HW_LED1             (pin_A1) // red
+#define MICROPY_HW_LED2             (pin_A2) // green
+#define MICROPY_HW_LED3             (pin_A3) // blue
+#define MICROPY_HW_LED1_PWM         { TIM2, 2, TIM_CHANNEL_2, GPIO_AF1_TIM1 }
+#define MICROPY_HW_LED2_PWM         { TIM2, 2, TIM_CHANNEL_3, GPIO_AF1_TIM1 }
+#define MICROPY_HW_LED3_PWM         { TIM2, 2, TIM_CHANNEL_4, GPIO_AF1_TIM1 }
+#define MICROPY_HW_LED_ON(pin)      (mp_hal_pin_low(pin))
+#define MICROPY_HW_LED_OFF(pin)     (mp_hal_pin_high(pin))
+
+// USB config
+#define MICROPY_HW_USB_FS (1)
